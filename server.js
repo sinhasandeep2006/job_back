4664e43df4e2ddb53922bdcdab7efd1f7aaf5a9f
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-08-30 14:04:59 +0530

    maked

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://job-front-eight.vercel.app/',
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
